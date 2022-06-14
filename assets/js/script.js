398dbdaf7606f8a6a3fd2cdb2fdd4d4eb4530e72
Sadee <mohammadsadee24@gmail.com>
2022-06-14 19:15:49 +0600

    add product-details page

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,3 +70,24 @@ window.addEventListener("scroll", function () {
   window.scrollY >= 100 ? backTopBtn.classList.add("active")
     : backTopBtn.classList.remove("active");
 });
+
+
+
+/**
+ * product details page
+ */
+
+const productDisplay = document.querySelector("[data-product-display]");
+const productThumbnails = document.querySelectorAll("[data-product-thumbnail]");
+
+for (let i = 0; i < productThumbnails.length; i++) {
+  productThumbnails[i].addEventListener("click", function () {
+    productDisplay.src = this.src;
+    productDisplay.classList.add("fade-anim");
+
+    setTimeout(function () {
+      productDisplay.classList.remove("fade-anim");
+    }, 250);
+
+  });
+}
